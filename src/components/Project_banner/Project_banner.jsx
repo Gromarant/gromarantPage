65d111deeca7d4e8607a207f23db19f7474f4e42
Mariangelica Rodriguez <gromarant@gmail.com>
2024-01-10 20:03:05 +0100

    add horizontal line as divisor

--- src/components/Project_banner/Project_banner.jsx
+++ src/components/Project_banner/Project_banner.jsx
@@ -5,31 +5,36 @@ const rrss = ['Linkedin', 'Instagram', 'Twitter'];
 
 function Project_banner({projects}) {
 
-
   return(
     <>
-      { projects?.map((project, index) =>
-        <section key={`${project.title}_${index}`}>
-          <article className={`project_banner ${index % 2 === 0 ? 'img-text' : 'text-img'}`}>
-            <section className={`banner_img ${project.img}`}>
-              { project.btns?.map(link => <button className={`banner_btn ${link}`} key={`${project.title}_${link}`}>{link}</button>)}
-            </section>
+      { projects?.map((project, index) => {
+        const isNotLast = index < projects?.length-1;
+        
+          return (
+          <section key={`${project.title}_${index}`}>
+            <article className={`project_banner ${index % 2 === 0 ? 'img-text' : 'text-img'}`}>
+              <section className={`banner_img ${project.img}`}>
+                { project.btns?.map(link => <button className={`banner_btn ${link}`} key={`${project.title}_${link}`}>{link}</button>)}
+              </section>
 
-            <section className='banner_desc'>
-              <h1 className='banner_title'>{project.title}</h1>
-              <p className='banner_textContent'>{project.desc}</p>
-              <h2>Funcionalities</h2>
-              <ul className='banner_functionalities'>
-                { project.functionalities?.map( func => {
-                  <li key={`${project.title}_${func}`}>{func}</li>
-                })}
-              </ul>
-              <p>develop by: {project.copyRight}</p>
-            </section>
-          </article>
-          <RRSS rrss={rrss}/>
-        </section>
-      )} 
+              <section className='banner_desc'>
+                <h1 className='banner_title'>{project.title}</h1>
+                <p className='banner_textContent'>{project.desc}</p>
+                <h2>Funcionalities</h2>
+                <ul className='banner_functionalities'>
+                  { project.functionalities?.map( func => {
+                    <li key={`${project.title}_${func}`}>{func}</li>
+                  })}
+                </ul>
+                <p>develop by: {project.copyRight}</p>
+              </section>
+            </article>
+            <RRSS rrss={rrss}/>
+
+            {(isNotLast) ? <span className='divisor'>---------------------------------------</span> : null}
+          </section>
+          )
+      })} 
     </>
   )
 }
