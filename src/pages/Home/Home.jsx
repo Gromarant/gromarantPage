88e7cda7535a02d9c96da118bdc5fec05fd1957c
Mariangelica Rodriguez <gromarant@gmail.com>
2024-01-11 11:36:58 +0100

    store projects data into a json file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,42 +1,7 @@
 import Project_banner from '../../components/Project_banner/Project_banner';
-
-const projects = [
-  {
-    "img": "FullPokeApp",
-    "btns": ['Prototype', 'Web', 'Repository'],
-    "title": "FullPokeApp",
-    "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore sed expedita eos accusantium ea repellendus neque illo animi laboriosam consequatur cupiditate eligendi dolorum, aliquid, earum delectus unde praesentium nobis?",
-    "functionalities" : ['func_1', 'func_2', 'func_3'],
-    "copyRight":'Mariangelica Rodriguez',
-  },
-  {
-    "img": "Groma-Snake",
-    "btns": ['Prototype', 'Web', 'Repository'],
-    "title": "Groma-Snake",
-    "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore sed expedita eos accusantium ea repellendus neque illo animi laboriosam consequatur cupiditate eligendi dolorum, aliquid, earum delectus unde praesentium nobis?",
-    "functionalities" : ['func_1', 'func_2', 'func_3'],
-    "copyRight":'Mariangelica Rodriguez',
-  },
-  {
-    "img": "Groma-Snake",
-    "btns": ['Prototype', 'Web', 'Repository'],
-    "title": "Groma-Snake2",
-    "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore sed expedita eos accusantium ea repellendus neque illo animi laboriosam consequatur cupiditate eligendi dolorum, aliquid, earum delectus unde praesentium nobis?",
-    "functionalities" : ['func_1', 'func_2', 'func_3'],
-    "copyRight":'Mariangelica Rodriguez',
-  },
-  {
-    "img": "Groma-Snake",
-    "btns": ['Prototype', 'Web', 'Repository'],
-    "title": "Groma-Snake3",
-    "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore sed expedita eos accusantium ea repellendus neque illo animi laboriosam consequatur cupiditate eligendi dolorum, aliquid, earum delectus unde praesentium nobis?",
-    "functionalities" : ['func_1', 'func_2', 'func_3'],
-    "copyRight":'Mariangelica Rodriguez',
-  },
-]
+import projects from "../../data/projects.json";
 
 function Home() {
-
   return (
     <>
       <Project_banner projects={projects} />
