38fde646a33a71ec45b063c57557728358ed428b
Mariangelica Rodriguez <gromarant@gmail.com>
2024-01-11 20:06:00 +0100

    assemble the Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,28 @@
-import Project_banner from '../../components/Project_banner/Project_banner';
+import RRSS from '../../components/RRSS/RRSS.jsx';
+import Banner from '../../components/Banner/Banner.jsx';
+import Text_block from '../../components/Text_block/Text_block.jsx';
+import rrss from "../../data/rrss.json";
 import projects from "../../data/projects.json";
+import {isEven, isNotLast} from "../../utils.js";
+
 
 function Home() {
   return (
     <>
-      <Project_banner projects={projects} />
+      { projects?.map((project, index) => {
+          return (
+          <section key={`${project.title}_${index}`}>
+            <Banner className={`project_banner ${ isEven ? 'img-text' : 'text-img'}`}
+              img={project.home_img}
+              btns={project.btns}
+              title={project.title}
+            />
+            <Text_block title={project.title} desc={project.desc} func={project.functionalities} authors={project.copyRight}/>
+            <RRSS rrss={rrss}/>
+            {(isNotLast( projects, index)) ? <span className='divisor'>---------------------------------------</span> : null}
+          </section>
+          )
+      })} 
     </>
   )
 }
