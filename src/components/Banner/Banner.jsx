f3db31f4880ef3f7ad97a2e435b3dd4c7f2c25ce
Mariangelica Rodriguez <gromarant@gmail.com>
2024-01-11 11:35:28 +0100

    add key properties into Banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,13 @@
-function Banner({img, btns, key, title=''}) {
+function Banner({img, btns, title=''}) {
   const hasTitle = title !== '';
 
   return(
     <>
       <article className={`project_banner`}>
-        { hasTitle ?<h1>{title}</h1> : null}
+        { hasTitle ?<h1 key={`title_${title}`}>{title}</h1> : null}
+        
         <section className={`banner_img ${img}`}>
-          { btns?.map(link => <button className={`banner_btn ${link}`} key={key}>{link}</button>)}
+          { btns?.map(link => <button className={`banner_btn ${link}`}  key={`btn_${link}`}>{link}</button>)}
         </section>
       </article>
     </>
