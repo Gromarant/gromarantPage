1950c920d9e908af3c28dc57536c28a4550e74ba
Mariangelica Rodriguez <gromarant@gmail.com>
2024-01-15 14:18:05 +0100

    add mailto href into <a> tag

--- src/components/main_components/Footer/Footer.jsx
+++ src/components/main_components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
       <img src='https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=91,h=63,fit=crop/m5KpR2QMglIWRz11/logo-short-medium-2023-AMqVRn6paDio9xDP.png' alt='Gromarant logo'/>
       <p>© Mariangelica Rodríguez</p>
-      <a href="http://" target="_blank">contacto@gromarant.com</a>
+      <a href="mailto:contacto@gromarant.com" target="_blank">contacto@gromarant.com</a>
       <FaGithub />
     </footer>
   )
