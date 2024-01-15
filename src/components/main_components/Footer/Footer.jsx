9bbd5051c3e4d0fda082b38fa7dcffb178b8c5b3
Mariangelica Rodriguez <gromarant@gmail.com>
2024-01-15 13:15:50 +0100

    add Footer component

---
+++ src/components/main_components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { FaGithub } from "react-icons/fa";
+
+function Footer() {
+  return(
+    <footer>
+      <img src='https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=91,h=63,fit=crop/m5KpR2QMglIWRz11/logo-short-medium-2023-AMqVRn6paDio9xDP.png' alt='Gromarant logo'/>
+      <p>© Mariangelica Rodríguez</p>
+      <a href="http://" target="_blank">contacto@gromarant.com</a>
+      <FaGithub />
+    </footer>
+  )
+}
+
+export default Footer;
