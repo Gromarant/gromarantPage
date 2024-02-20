ecdc8f375574505ecf545d753ae75bbe750f51c7
Mariangelica Rodriguez <gromarant@gmail.com>
2024-02-20 09:29:59 +0100

    update projects banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,25 @@
-function Banner({img, btns, title=''}) {
-  const hasTitle = title !== false;
+import { isTheSameAs } from "../../utils";
+import { FaGithub, FaPlayCircle, FaGlobe  } from "react-icons/fa";
+import { MdDevices } from "react-icons/md";
+
+function Banner({img, btns, title}) {
 
   return(
     <>
       <article className={`project_banner`}>
-        { hasTitle ? <h1>{title}</h1> : null}
+        { title ? <h1>{title}</h1> : null}
         
         <section className={`banner_img ${img}`}>
-          { btns?.map(btn => <a key={`btn_${btn.name}`} href={btn.link} target='_blank'><button className={`banner_btn ${btn.name}`}>{btn.name}</button></a>)}
+          <section className='banner_btns'>
+            { btns?.map(btn => <a key={`btn_${btn.name}`} href={btn.link} target='_blank'><button className={`banner_btn ${btn.name}`}>
+              { 
+                isTheSameAs(btn.name , 'Repository') || isTheSameAs(btn.name , 'Repositorio') ? <FaGithub />
+                : isTheSameAs(btn.name , 'Web') ? <FaGlobe />
+                : isTheSameAs(btn.name , 'Prototype') || isTheSameAs(btn.name , 'Prototipo') ? <MdDevices />
+                : isTheSameAs(btn.name , 'Video') ? <FaPlayCircle /> : null
+              }
+              {btn.name}</button></a>)}
+          </section>
         </section>
       </article>
     </>
