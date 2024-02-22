3a62981fb04c69b7b9c5daddb86b3f48c2c4e32a
Mariangelica Rodriguez <gromarant@gmail.com>
2024-02-22 22:32:35 +0100

    update projects banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,16 @@
 import { isTheSameAs } from "../../utils";
 import { FaGithub, FaPlayCircle, FaGlobe  } from "react-icons/fa";
 import { MdDevices } from "react-icons/md";
+import {isEven} from "../../utils";
 
-function Banner({img, btns, title}) {
+function Banner({img, btns, index, title}) {
 
   return(
     <>
-      <article className={`project_banner`}>
+      <article className='project_banner'>
         { title ? <h1>{title}</h1> : null}
         
-        <section className={`banner_img ${img}`}>
+        <section className={`banner_img ${ isEven(index) ? 'btns_img' : 'img_btns'} ${img}`}>
           <section className='banner_btns'>
             { btns?.map(btn => <a key={`btn_${btn.name}`} href={btn.link} target='_blank'><button className={`banner_btn ${btn.name}`}>
               { 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,47 +1,52 @@
 import RRSS from '../../components/RRSS/RRSS.jsx';
 import Banner from '../../components/Banner/Banner.jsx';
 import Text_block from '../../components/Text_block/Text_block.jsx';
-import {isEven} from "../../utils.js";
+import { isEven } from "../../utils.js";
+
 
 function Home({translate}) {
 
   return (
     <>
       <section className='projects'>
-        <article className='project'>
-          <Banner className={`project_banner ${ isEven ? 'img-text' : 'text-img'}`}
+        <article className={`project ${ isEven(0) ? 'desc_banner' : 'banner_desc'}`}>
+          <Banner
             img={translate.fullPokeApp_title}
             btns={translate.fullPokeApp_links}
+            index={0}
           />
           <Text_block title={translate.fullPokeApp_title} desc={translate.fullPokeApp_desc} func={translate.fullPokeApp_functionalities} authors={translate.fullPokeApp_copyRight}/>
         </article>
         <RRSS rrss={translate.rrss}/>
         <span className='divisor'></span>
 
-        <article className='project'>
-          <Banner className={`project_banner ${ isEven ? 'img-text' : 'text-img'}`}
+        <article className={`project ${ isEven(1) ? 'desc_banner' : 'banner_desc'}`}>
+          <Banner
             img={translate.gromaSnake_title}
             btns={translate.gromaSnake_links}
+            index={1}
           />
           <Text_block title={translate.gromaSnake_title} desc={translate.gromaSnake_desc} func={translate.gromaSnake_functionalities} authors={translate.gromaSnake_copyRight}/>
         </article>      
         <RRSS rrss={translate.rrss}/>
         <span className='divisor'></span>
 
-        <article className='project'>
-          <Banner className={`project_banner ${ isEven ? 'img-text' : 'text-img'}`}
+        <article className={`project ${ isEven(2) ? 'desc_banner' : 'banner_desc'}`}>
+          <Banner
             img={translate.funFace_title}
             btns={translate.funFace_links}
+            index={2}
           />
           <Text_block title={translate.funFace_title} desc={translate.funFace_desc} func={translate.funFace_functionalities} authors={translate.funFace_copyRight}/>
         </article>    
         <RRSS rrss={translate.rrss}/>
         <span className='divisor'></span>
 
-        <article className='project'>
-          <Banner className={`project_banner ${ isEven ? 'img-text' : 'text-img'}`}
+        <article className={`project ${ isEven(3) ? 'desc_banner' : 'banner_desc'}`}>
+          <Banner
             img={translate.robopage_title}
             btns={translate.robopage_links}
+            index={3}
           />
           <Text_block title={translate.robopage_title} desc={translate.robopage_desc} func={translate.robopage_functionalities} authors={translate.robopage_copyRight}/>
         </article>
