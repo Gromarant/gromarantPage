6817cec56ebc8039739045179fdb95bf22ab1b45
Mariangelica Rodriguez <gromarant@gmail.com>
2024-02-27 11:25:01 +0100

    update/ component Profile structure, data and style

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,17 @@
 function Profile({photo, photo_alt, photo_title, author, profession, greating, greating_alt, greating_title}) {
+  console.log(profession);
   return(
     <article className='profile'>
-      <img src={photo} alt={photo_alt} title={photo_title}/>
-      <h1>{author}</h1>
-      <p>{profession}</p>
-      <img src={greating} alt={greating_alt} title={greating_title}/>
+      <img className='profile_photo' src={photo} alt={photo_alt} title={photo_title}/>
+      <section className='profile_text'>
+        <h1 className='profile_name'>{author}</h1>
+        <article className='profile_professions'>
+          <p>{profession["fullstack"]}</p>
+          <span>|</span>
+          <p>{profession["ux/ui"]}</p>
+        </article>
+      </section>
+      <img className='profile_greeting' src={greating} alt={greating_alt} title={greating_title}/>
     </article>
   )
 }
