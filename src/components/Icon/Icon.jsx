edd0c62f452b69f3a58d458e746b34b74ccd7c53
Mariangelica Rodriguez <gromarant@gmail.com>
2024-03-04 17:42:08 +0100

    set/ languages_tools icons display

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,7 +1,9 @@
 function Icon({img, name}) {
   return(
-    <article>
-      <img className='lang-tool' src={img} alt={name} title={name}/>
+    <article className='tools'>
+      <section className='tool_bg'>
+        <img className='lang-tool' src={img} alt={name} title={name}/>
+      </section>
       <p>{name}</p>
     </article>
   )
