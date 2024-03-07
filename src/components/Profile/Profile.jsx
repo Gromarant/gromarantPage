2e8867c09343b735aefc16fc20cd6f82f4f5dbb8
Mariangelica Rodriguez <gromarant@gmail.com>
2024-03-07 13:35:38 +0100

    update/ Profile component styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,27 @@
-function Profile({photo, photo_alt, photo_title, author, profession, greating, greating_alt, greating_title}) {
-  console.log(profession);
+function Profile({
+  photo_small, 
+  photo_medium, 
+  photo_large, 
+  photo_alt, 
+  photo_title, 
+  author, 
+  profession, 
+  greating_small, 
+  greating_large, 
+  greating_alt, 
+  greating_title
+}) {
   return(
     <article className='profile'>
-      <img className='profile_photo' src={photo} alt={photo_alt} title={photo_title}/>
+      <img className='profile_photo' 
+        src={photo_small}
+        srcSet={
+                `${photo_small} 120w,
+                 ${photo_medium} 144w,
+                 ${photo_large} 168w`
+               }
+        alt={photo_alt} 
+        title={photo_title}/>
       <section className='profile_text'>
         <h1 className='profile_name'>{author}</h1>
         <article className='profile_professions'>
@@ -11,7 +30,14 @@ function Profile({photo, photo_alt, photo_title, author, profession, greating, g
           <p>{profession["ux/ui"]}</p>
         </article>
       </section>
-      <img className='profile_greeting' src={greating} alt={greating_alt} title={greating_title}/>
+      <img className='profile_greeting' 
+        src={greating_small}
+        srcSet={
+                `${greating_small},
+                 ${greating_large}`
+               }
+               alt={greating_alt} 
+               title={greating_title}/>
     </article>
   )
 }
