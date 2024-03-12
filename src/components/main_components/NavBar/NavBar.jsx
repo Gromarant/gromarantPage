2342d439c7860a0c5d1e74499e0e09664307be93
Mariangelica Rodriguez <gromarant@gmail.com>
2024-03-12 12:26:30 +0100

    Fix/ dropdown menu events to display

--- src/components/main_components/NavBar/NavBar.jsx
+++ src/components/main_components/NavBar/NavBar.jsx
@@ -2,22 +2,28 @@ import { Link, NavLink } from 'react-router-dom';
 import { FaBars } from "react-icons/fa6";
 import { LangContext } from '../../../context/LangContext';
 import { lang_en } from "../../../utils";
-import { useContext } from 'react';
+import { useContext, useRef, useState } from 'react';
 import Dropdown from '../../Dropdown/Dropdown';
 
 function NavBar() {
   const { lang } = useContext(LangContext);
+  const [ open, setOpen ] = useState(false);
 
-  const handleMenuClick = () => {
-    const menuDropdown = document.querySelector('.dropdown');
-    menuDropdown.classList.toggle('menu');
+  const dropdownRef = useRef();
+
+  const handleMenu = (e) => {
+    if (!dropdownRef.current.contains(e.target) && e.target !== document.querySelector('.burguer_menu')) {
+      setOpen(false);
+    }
   }
+  document.addEventListener('mousedown', handleMenu);
+  
 
   return(
     <nav className='navBar'>
       <Link to='/'><img className='navBar_logo' src='images/navBar_logo.svg' alt='Logo gromarant' title='Gromarant'/></Link>
-      <FaBars className='icon burguer_menu' onClick={handleMenuClick}/>
-      <section className='dropdown'>
+      <FaBars className='icon burguer_menu' onClick={() => setOpen(!open)}/>
+      <section className={`dropdown ${open ? 'menu' : ''}`} ref={dropdownRef}>
         <Dropdown/>
         { lang_en(lang)
           ? <ul className='dropdown_links'>
