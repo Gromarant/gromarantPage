65983acd26f322e75e3b45985734bd65e69f5d39
Mariangelica Rodriguez <gromarant@gmail.com>
2024-03-14 23:09:28 +0100

    refactor/Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,13 +5,15 @@ function Banner({img, btns, index, title}) {
   return(
     <>
       <article className='project_banner'>
-        { title ? <h1>{title}</h1> : null}
+        { title && <h1>{title}</h1> }
         
         <section className={`banner_img ${ isEven(index) ? 'btns_img' : 'img_btns'} ${img}`}>
           <section className='banner_btns'>
-            { btns?.map(btn => <a key={`btn_${btn.name}`} href={btn.link} target='_blank'>
-                                  <button className={`btn ${btn.name}`}><btn.icon />{btn.name}</button>
-                               </a>)
+            { btns?.map(btn => (
+              <a key={`btn_${btn.name}`} href={btn.link} target='_blank'>
+                <button className={`btn ${btn.name}`}><btn.icon />{btn.name}</button>
+              </a>
+            ))
             }
           </section>
         </section>
