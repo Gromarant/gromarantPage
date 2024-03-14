92f1428d51af7714ff115480352fdbbe463544ef
Mariangelica Rodriguez <gromarant@gmail.com>
2024-03-14 23:11:27 +0100

    refactor/Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,62 +1,51 @@
 import RRSS from '../../components/RRSS/RRSS.jsx';
 import Banner from '../../components/Banner/Banner.jsx';
-import Text_block from '../../components/Text_block/Text_block.jsx';
 import { isEven } from "../../utils.js";
 
 
 function Home({translate}) {
   return (
-    <>
-      <section className='projects'>
-        <article className={`project ${ isEven(0) ? 'banner_desc' : 'desc_banner'}`}>
-          <Banner
-            img={translate.fullPokeApp_title}
-            btns={translate.fullPokeApp_links}
-            index={0}
-          />
-          <Text_block title={translate.fullPokeApp_title} desc={translate.fullPokeApp_desc} func={translate.fullPokeApp_functionalities} authors={translate.fullPokeApp_copyRight}/>
-        </article>
-        <section className='rrss'>
-          <RRSS rrss={translate.rrss}/>
-        </section>
-        <span className='divisor'></span>
+    <section className='projects'>
+      {
+        translate.projects.map((project, projectIndex) => {
+          const isNotLastProject = projectIndex < translate.projects.length -1;
 
-        <article className={`project ${ isEven(1) ? 'banner_desc' : 'desc_banner'}`}>
-          <Banner
-            img={translate.gromaSnake_title}
-            btns={translate.gromaSnake_links}
-            index={1}
-          />
-          <Text_block title={translate.gromaSnake_title} desc={translate.gromaSnake_desc} func={translate.gromaSnake_functionalities} authors={translate.gromaSnake_copyRight}/>
-        </article>      
-        <section className='rrss'>
-          <RRSS rrss={translate.rrss}/>
-        </section>
-        <span className='divisor'></span>
+          return (
+            <section  key={`project-${project.title}`}>
+              <article className={`project ${isEven(0) ? 'banner_desc' : 'desc_banner'}`}>
+                <Banner
+                  img={project.title}
+                  btns={project.links}
+                  index={projectIndex} />
+                <article className='project_desc'>
+                  <section className='desc_title'>
+                    <h1>{project.title}</h1>
+                    <p>{project.desc}</p>
+                  </section>
 
-        <article className={`project ${ isEven(2) ? 'banner_desc' : 'desc_banner'}`}>
-          <Banner
-            img={translate.funFace_title}
-            btns={translate.funFace_links}
-            index={2}
-          />
-          <Text_block title={translate.funFace_title} desc={translate.funFace_desc} func={translate.funFace_functionalities} authors={translate.funFace_copyRight}/>
-        </article>    
-        <section className='rrss'>
-          <RRSS rrss={translate.rrss}/>
-        </section>
-        <span className='divisor'></span>
-
-        <article className={`project ${ isEven(3) ? 'banner_desc' : 'desc_banner'}`}>
-          <Banner
-            img={translate.robopage_title}
-            btns={translate.robopage_links}
-            index={3}
-          />
-          <Text_block title={translate.robopage_title} desc={translate.robopage_desc} func={translate.robopage_functionalities} authors={translate.robopage_copyRight}/>
-        </article>
-      </section>
-    </>
+                  <section className='desc_func'>
+                    <h2>{project.subtitle}:</h2>
+                    <ul>
+                      { project.functionalities?.map((functionality, index) => <li key={`${project.title}_functionalitie_${index}`}>{functionality}</li>)}
+                    </ul>
+                  </section>
+                  <section className='desc_authors'>
+                    <p className='dev'>{project.developed}</p>
+                    { project.copyRight?.map((author, index) => <p className='authors' key={`${project.title}_${author}_${index}`}>{author}</p>)}
+                  </section>
+                </article>
+              </article>
+              <section className='rrss'>
+                <RRSS rrss={translate.rrss} />
+              </section>
+              {
+                isNotLastProject && <span className='divisor'></span>
+              }
+            </section>
+          )
+        })
+      }
+    </section>
   )
 }
 
