8e2833c27d9ca221fe8dff7b4e398052657c62d4
Mariangelica Rodriguez <gromarant@gmail.com>
2024-03-14 23:08:51 +0100

    refactor/data spanish language

--- src/data/es.js
+++ src/data/es.js
@@ -5,236 +5,266 @@ import { FaXTwitter } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 
 const spanish = {
-  "fullPokeApp_title": "FullPokeApp",
-  "fullPokeApp_links": [
+  projects: [
     {
-      "name": "Repositorio",
-      "link": "https://github.com/Gromarant/fullPokeApp",
-      "icon": FaGithub
+      title: 'FullPokeApp',
+      links: [
+        {
+          name: "Repositorio",
+          link: "https://github.com/Gromarant/fullPokeApp",
+          icon: FaGithub
+        },
+        {
+          name: "Web",
+          link: "https://fullpokeapp.netlify.app/",
+          icon: FaGlobe
+        }
+      ],
+      desc: 'Aplicación web desarrollada en React.js, SASS y Nextui.',
+      subtitle: "Funciones",
+      functionalities: [ "Juego de memoria.", "Buscador.", "Sección para crear Pokémones."],
+      developed: "Desarrollado por",
+      copyRight: ["Mariangelica Rodriguez"]
     },
     {
-      "name": "Web",
-      "link": "https://fullpokeapp.netlify.app/",
-      "icon": FaGlobe
-    }
-  ],
-  "fullPokeApp_desc": "Aplicación web desarrollada en React.js, SASS y Nextui.",
-  "fullPokeApp_functionalities" : [ "Juego de memoria.", "Buscador.", "Sección para crear Pokémones."],
-  "fullPokeApp_copyRight":["Mariangelica Rodriguez"],
-
-  "gromaSnake_title": "Groma-Snake",
-  "gromaSnake_links": [
-    {
-      "name": "Prototipo",
-      "link": "https://www.figma.com/file/6SYeCJMoiDG8LZPKQ3QRyZ/GromaSnake?type=design&node-id=0-1&mode=design&t=taUbdkRK095LZ2Cp-0",
-      "icon": MdDevices
+      title: 'Groma-Snake',
+      links: [
+        {
+          name: "Prototipo",
+          link: "https://www.figma.com/file/6SYeCJMoiDG8LZPKQ3QRyZ/GromaSnake?type=design&node-id=0-1&mode=design&t=taUbdkRK095LZ2Cp-0",
+          icon: MdDevices
+        },
+        {
+          name: "Repositorio",
+          link: "https://github.com/Gromarant/GromaSnake",
+          icon: FaGithub
+        },
+        {
+          name: "Web",
+          link: "https://gromarant.github.io/GromaSnake/",
+          icon: FaGlobe
+        },
+        {
+          name: "Video",
+          link: "https://www.youtube.com/watch?v=hwLYrtQhLVI",
+          icon: FaPlayCircle
+        }
+      ],
+      desc: 'Juego desarrollado en  HTML, CSS y JavaScript vanilla.',
+      subtitle: "Funciones",
+      functionalities: [ "Juego de la culebra.", "Reiniciar el juego.", "er Puntaje y score."],
+      developed: "Desarrollado por",
+      copyRight: ["Mariangelica Rodriguez"]
     },
     {
-      "name": "Repositorio",
-      "link": "https://github.com/Gromarant/GromaSnake",
-      "icon": FaGithub
+      title: 'FunFace',
+      links: [
+        {
+          name: "Prototipo",
+          link: "https://www.figma.com/proto/fV01fBdBQbbT5fScqf7ro1/FunFace?page-id=15%3A772&node-id=15-933&viewport=758%2C109%2C0.05&scaling=scale-down&starting-point-node-id=15%3A1603",
+          icon: MdDevices
+        },
+        {
+          name: "Repositorio",
+          link: "https://github.com/Gromarant/FunFace",
+          icon: FaGithub
+        },
+        {
+          name: "Web",
+          link: "https://gromarant.github.io/FunFace/",
+          icon: FaGlobe
+        }
+      ],
+      desc: 'Página web estática, desarrollada en HTML, CSS y JavaScript vanilla.',
+      subtitle: "Funciones",
+      functionalities: [ "Mostrar un emoji aleatorio.", "Elegir emoji a mostrar desde selector."],
+      developed: "Desarrollado por",
+      copyRight: ["Mariangelica Rodriguez"]
     },
     {
-      "name": "Web",
-      "link": "https://gromarant.github.io/GromaSnake/",
-      "icon": FaGlobe
-    },
-    {
-      "name": "Video",
-      "link": "https://www.youtube.com/watch?v=hwLYrtQhLVI",
-      "icon": FaPlayCircle
-    }
-  ],
-  "gromaSnake_desc": "Juego desarrollado en  HTML, CSS y JavaScript vanilla.",
-  "gromaSnake_functionalities" : [ "Juego de la culebra.", "Reiniciar el juego.", "er Puntaje y score."],
-  "gromaSnake_copyRight":["Mariangelica Rodriguez"],
-
-  "funFace_title": "FunFace",
-  "funFace_links": [
-    {
-      "name": "Prototipo",
-      "link": "https://www.figma.com/proto/fV01fBdBQbbT5fScqf7ro1/FunFace?page-id=15%3A772&node-id=15-933&viewport=758%2C109%2C0.05&scaling=scale-down&starting-point-node-id=15%3A1603",
-      "icon": MdDevices
-    },
-    {
-      "name": "Repositorio",
-      "link": "https://github.com/Gromarant/FunFace",
-      "icon": FaGithub
-    },
-    {
-      "name": "Web",
-      "link": "https://gromarant.github.io/FunFace/",
-      "icon": FaGlobe
-    }
-  ],
-  "funFace_desc": "Página web estática, desarrollada en HTML, CSS y JavaScript vanilla.",
-  "funFace_functionalities" : [ "Mostrar un emoji aleatorio.", "Elegir emoji a mostrar desde selector."],
-  "funFace_copyRight":["Mariangelica Rodriguez"],
-
-  "robopage_title": "Robopage",
-  "robopage_links": [
-    {
-      "name": "Repositorio",
-      "link": "https://github.com/Gromarant/Robopage",
-      "icon": FaGithub
-    },
-    {
-      "name": "Web",
-      "link": "https://robopage.netlify.app/",
-      "icon": FaGlobe
-    }
-  ],
-  "robopage_desc": "Aplicación web en React.js",
-  "robopage_functionalities" : [ "Buscador con solicitud de datos a una Api externa."],
-  "robopage_copyRight":["Mariangelica Rodriguez"],
-
-  "rrss" : [
-    {
-      "name": "LinkedIn",
-      "link": "https://www.linkedin.com/in/mariangelica-rodr%C3%ADguez-p%C3%A9rez/",
-      "title": "Ir a LinkedIn",
-      "icon": FaLinkedinIn
-    },
-    {
-      "name": "Instagram",
-      "link": "https://www.instagram.com/gromarant/",
-      "title": "Ir a Instagram",
-      "icon": FaInstagram
-    },
-    {
-      "name": "Twitter",
-      "link": "https://twitter.com/GromarantDev",
-      "title": "Ir a Twitter",
-      "icon": FaXTwitter
+      title: 'Robopage',
+      links: [
+        {
+          name: "Repositorio",
+          link: "https://github.com/Gromarant/Robopage",
+          icon: FaGithub
+        },
+        {
+          name: "Web",
+          link: "https://robopage.netlify.app/",
+          icon: FaGlobe
+        }
+      ],
+      desc: 'Aplicación web en React.js',
+      subtitle: "Funciones",
+      functionalities: [ "Buscador con solicitud de datos a una Api externa."],
+      developed: "Desarrollado por",
+      copyRight: ["Mariangelica Rodriguez"]
     }
   ],
 
-  "mailTo": {
-    "name": "Email",
-    "link": "mailto:contacto@gromarant.com",
-    "title": "Escribir un correo",
-    "icon": MdEmail
+  rrss : [
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/mariangelica-rodr%C3%ADguez-p%C3%A9rez/",
+      title: "Ir a LinkedIn",
+      icon: FaLinkedinIn
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/gromarant/",
+      title: "Ir a Instagram",
+      icon: FaInstagram
+    },
+    {
+      name: "Twitter",
+      link: "https://twitter.com/GromarantDev",
+      title: "Ir a Twitter",
+      icon: FaXTwitter
+    }
+  ],
+
+  mailTo: {
+    name: "Email",
+    link: "mailto:contacto@gromarant.com",
+    title: "Escribir un correo",
+    icon: MdEmail
   },
 
-  "profile_photo_src": {
-    "small": "images/profile_photo120.svg",
-    "medium": "images/profile_photo144.svg",
-    "large": "images/profile_photo168.svg"
+  profile_photo : {
+    src: {
+      small: "images/profile_photo120.svg",
+      medium: "images/profile_photo144.svg",
+      large: "images/profile_photo168.svg"
+    },
+    alt: "Foto de perfil"
   },
-  "profile_photo_alt": "Foto de perfil",
 
-  "author_name": "Mariangelica Rodriguez",
-  "author_profession": {
-    "fullstack": "Desarrolladora Full stack",
-    "ux/ui": "Diseñadora UI/UX"
+  author: {
+    name: "Mariangelica Rodriguez",
+    profession: ["Desarrolladora Full stack", "Diseñadora UI/UX"],
+    greating: {
+      src: {
+        small: "images/profile_greeting_card_es_small.svg",
+        large: "images/profile_greeting_card_es_large.svg"
+      },
+      alt: "Hola!! Encantada de que estés por aquí y que conozcas un poco sobre mí",
+      title: "bienvenid@!!"
+    }
   },
-  "author_greating_src": {
-    "small": "images/profile_greeting_card_es_small.svg",
-    "large": "images/profile_greeting_card_es_large.svg"
-  },
-  "author_greating_alt": "Hola!! Encantada de que estés por aquí y que conozcas un poco sobre mí",
-  "author_greating_title": "bienvenid@!!",
  
-  "langs_and_tools": [
+  langs_and_tools: [
     {
-      "name": "Javascript",
-      "url": "images/icons/icon_js_small.svg"
+      name: "Javascript",
+      url: "images/icons/icon_js_small.svg"
     },
     {
-      "name": "React.Js",
-      "url": "images/icons/icon_react_small.svg"
+      name: "React.Js",
+      url: "images/icons/icon_react_small.svg"
     },
     {
-      "name": "Node.js",
-      "url": "images/icons/icon_nodejs_small.svg"
+      name: "Node.js",
+      url: "images/icons/icon_nodejs_small.svg"
     },
     {
-      "name": "HTML5",
-      "url": "images/icons/icon_html5_small.svg"
+      name: "HTML5",
+      url: "images/icons/icon_html5_small.svg"
     },
     {
-      "name": "SASS",
-      "url": "images/icons/icon_sass_small.svg"
+      name: "SASS",
+      url: "images/icons/icon_sass_small.svg"
     },
     {
-      "name": "Express.js",
-      "url": "images/icons/icon_expressjs_small.svg"
+      name: "Express.js",
+      url: "images/icons/icon_expressjs_small.svg"
     },
     {
-      "name": "CSS3",
-      "url": "images/icons/icon_css3_small.svg"
+      name: "CSS3",
+      url: "images/icons/icon_css3_small.svg"
     },
     {
-      "name": "Nextui",
-      "url": "images/icons/icon_nextui_small.svg"
+      name: "Nextui",
+      url: "images/icons/icon_nextui_small.svg"
     },
     {
-      "name": "Jest",
-      "url": "images/icons/icon_jest_small.svg"
+      name: "Jest",
+      url: "images/icons/icon_jest_small.svg"
     },
     {
-      "name": "GIT/ GitHub",
-      "url": "images/icons/icon_github_small.svg"
+      name: "GIT/ GitHub",
+      url: "images/icons/icon_github_small.svg"
     },
     {
-      "name": "Figma",
-      "url": "images/icons/icon_figma_small.svg"
+      name: "Figma",
+      url: "images/icons/icon_figma_small.svg"
     },
     {
-      "name": "Balsamic",
-      "url": "images/icons/icon_balsamic_small.svg"
+      name: "Balsamic",
+      url: "images/icons/icon_balsamic_small.svg"
     },
     {
-      "name": "Firebase",
-      "url": "images/icons/icon_firebase_small.svg"
+      name: "Firebase",
+      url: "images/icons/icon_firebase_small.svg"
     },
     {
-      "name": "MongoDb",
-      "url": "images/icons/icon_mongodb_small.svg"
+      name: "MongoDb",
+      url: "images/icons/icon_mongodb_small.svg"
     },
     {
-      "name": "Docker",
-      "url": "images/icons/icon_docker_small.svg"
+      name: "Docker",
+      url: "images/icons/icon_docker_small.svg"
     },
     {
-      "name": "SQL",
-      "url": "images/icons/icon_sql_small.svg"
+      name: "SQL",
+      url: "images/icons/icon_sql_small.svg"
     }
   ],
 
-  "section_experience_title": "Experiencia",
-  "section_Lang_title": "Lenguajes / herramientas",
-  "section_academic_title": "Titulaciones",
-
-  "academic_title": [
+  academic_title: [
     {
-      "title": "Desarrollador Full stack",
-      "date": "08/2023",
-      "academy": "The Bridge | Digital Talent Accelerator",
-      "diploma_link": "https://drive.google.com/file/d/17Tliv0fcZHBc5yvRQ8RAyOg80NLgF-eY/view?usp=sharing"
+      title: "Desarrollador Full stack",
+      date: "08/2023",
+      academy: "The Bridge | Digital Talent Accelerator",
+      diploma_link: "https://drive.google.com/file/d/17Tliv0fcZHBc5yvRQ8RAyOg80NLgF-eY/view?usp=sharing"
     },
     {
-      "title": "Diseñador UX/UI",
-      "date": "01/2021",
-      "academy": "The Bridge | Digital Talent Accelerator",
-      "diploma_link": "https://drive.google.com/file/d/1YrrCJn8vf9AQ1EBPqmh7G1oCbn7oYGjv/view"
+      title: "Diseñador UX/UI",
+      date: "01/2021",
+      academy: "The Bridge | Digital Talent Accelerator",
+      diploma_link: "https://drive.google.com/file/d/1YrrCJn8vf9AQ1EBPqmh7G1oCbn7oYGjv/view"
     },
     {
-      "title": "Web development",
-      "date": "07/2020",
-      "academy": "Centro de estudios Infer, SL (Sistema de Formación Profesional)",
-      "diploma_link": "https://drive.google.com/file/d/1TdUkbtT7b-GAhFzkx_daRuHY050DQZaU/view"
+      title: "Web development",
+      date: "07/2020",
+      academy: "Centro de estudios Infer, SL (Sistema de Formación Profesional)",
+      diploma_link: "https://drive.google.com/file/d/1TdUkbtT7b-GAhFzkx_daRuHY050DQZaU/view"
     }
   ],
 
-  "scroll_btn_title": "subir",
-
-  "location_title": "Dirección",
-  "Location_place": "Madrid, España",
-
-  "OnTheWeb_title": "En la web",
-
-  "copyright_title": "Derechos de autor"
+  text: {
+    navigation: {
+      links: {
+        project: "Proyectos",
+        about_me: "Sobre mi",
+      }
+    }, 
+    scroll: "subir",
+    footer: {
+      location: "Dirección",
+      place: "Madrid, España",
+      web: "En la web",
+      copyright: "Derechos de autor",
+      author: "© Mariangelica Rodríguez",
+      contact: "contacto"
+    },
+    about: {
+      sections: {
+        experience: "Experiencia",
+        languages: "Lenguajes / herramientas",
+        academic: "Titulaciones"
+      }
+    }
+  }
 }
 
 export default spanish;
