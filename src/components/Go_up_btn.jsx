ff28d2a16d821fb9d4e9b0d9ae40a4335d396b53
Gromarant <contacto@gromarant.com>
2024-03-26 15:18:31 +0000

    Build: (91c128d) Merge branch 'new_gromarant_page'

--- src/components/Go_up_btn.jsx
+++
@@ -1,9 +0,0 @@
-import { FaArrowCircleUp } from "react-icons/fa";
-
-function Go_up_btn({onClick, title}) {
-  return(
-    <button className='go_Up' onClick={onClick} title={title}><FaArrowCircleUp /></button>
-  )
-}
-
-export default Go_up_btn;
